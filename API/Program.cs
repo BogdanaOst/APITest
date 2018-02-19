4dae1582522987408e62882b4b65ad154ed6b304
BogdanaOst <bogdanochkaost@gmail.com>
2018-02-19 12:52:39 +0200

    prep

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,7 @@ namespace API
             RootObjectOrder Data = new RootObjectOrder()
             {
                 Source = "Something",
-                ApiDocumentId = "00000000-0000-0000-0000-000000000000",
+                ApiDocumentId = "00000000-0000-0000-0000-000000000001",
                 IsAsync = true,
                 Data = new Data()
                 {
