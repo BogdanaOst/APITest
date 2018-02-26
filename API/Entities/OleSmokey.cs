5a4c78c0ddf194bc753a74791b252796723ea809
BogdanaOst <bogdanochkaost@gmail.com>
2018-02-26 14:26:37 +0200

    OleSmokey Entity

---
+++ API/Entities/OleSmokey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class OleSmokey
+    {
+        public DateTime DateTimeS { get; set; }
+        public int ProductId { get; set; }
+        public float Quantity { get; set; }
+        public float Price { get; set; }
+        public string ProductName { get; set; }
+        public string DCSS { get; set; }
+        public string Department { get; set; }
+        public string Class { get; set; }
+        public string Subclass { get; set; }
+    }
+}
